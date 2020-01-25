2b064c1d4f8349a6f94cc753dc2c4ce146acdcac
hsaul <adrial.saul@gmail.com>
2020-01-25 10:06:54 -0500

    updated files

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "757587e0b8932ab68204e1935cbf5671"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBLfXzZFIuVkEyv34eGzNB1BnmGhjZk8c0"
